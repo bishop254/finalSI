0b4437ee86093ce82f58e1537888c71d26594485
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-01-31 16:24:28 +0300

    Added our newly created components to our routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', pathMatch: 'full', component: LandingComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
