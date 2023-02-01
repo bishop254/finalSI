f90a14198ade15cbaf2dc42afb1fc7fb0bdd6b09
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-02-01 09:59:07 +0300

    Ensured the landing page was dynamic based on the login status of the user.

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingComponent {
   /** Based on the screen size, switch from standard to one column per row */
@@ -23,5 +30,41 @@ export class LandingComponent {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private auth: AuthService,
+    private ref: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    if (this.auth.isLoggedIn) {
+      this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+        map(({ matches }) => {
+          if (matches) {
+            return [{ title: 'Landing', content: 'Welcome', cols: 2, rows: 2 }];
+          }
+
+          return [
+            { title: 'Landing', content: 'Welcome', cols: 1, rows: 2 },
+            { title: 'Logs', content: 'Logs', cols: 1, rows: 1 },
+          ];
+        })
+      );
+    } else {
+      this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+        map(({ matches }) => {
+          if (matches) {
+            return [{ title: 'Landing', content: 'Welcome', cols: 2, rows: 2 }];
+          }
+
+          return [
+            { title: 'Landing', content: 'Welcome', cols: 1, rows: 2 },
+            { title: 'Login', content: 'Login', cols: 1, rows: 1 },
+            { title: 'Logs', content: 'Logs', cols: 1, rows: 1 },
+          ];
+        })
+      );
+    }
+    this.ref.detectChanges();
+  }
 }
