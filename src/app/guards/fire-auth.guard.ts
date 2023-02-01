3c500941bfe2730610b2bd45d1f5365d2dad1dd3
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-02-01 09:57:05 +0300

    We then add a Auth guard to ensure only logged in users can visit certain pages.

---
+++ src/app/guards/fire-auth.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FireAuthGuard implements CanActivate {
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.isLoggedIn !== true) {
+      this.router.navigate(['landing']);
+      this.snackBar.open(
+        'You are not authorized to view this page',
+        'Auth Error'
+      );
+    }
+    return true;
+  }
+}
