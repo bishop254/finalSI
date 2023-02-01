06c7a31571388c7729b656cff376db29e90f57d1
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-02-01 09:56:15 +0300

    We add a new authentication service that connects to Firebase.
    We also put our configurations in the environment file.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import 'firebase/compat/auth';
+import { Router } from '@angular/router';
+
+import firebase from 'firebase/compat/app';
+import { environment } from 'src/environments/environment';
+
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+
+export interface User {
+  uid: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+  emailVerified: boolean;
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  userData: any;
+
+  constructor(private router: Router, private snackBar: MatSnackBar) {
+    firebase.initializeApp(environment.firebase);
+  }
+
+  async SignIn(email: string, password: string) {
+    try {
+      let loginAction = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+
+      console.log(loginAction.user?.providerData[0]);
+      this.router.navigate(['/users']);
+
+      this.snackBar.open('Login Success', 'Success', {
+        duration: 2 * 1000,
+      });
+
+      localStorage.setItem('userId', loginAction.user?.uid!);
+      localStorage.setItem('token', loginAction.user?.refreshToken!);
+      localStorage.setItem(
+        'userData',
+        JSON.stringify(loginAction.user?.providerData[0])
+      );
+
+      this.userData = loginAction.user;
+    } catch (error) {
+      this.snackBar.open('Login Failure/Invalid credentials', 'Failure', {
+        duration: 2 * 1000,
+      });
+    }
+  }
+
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('userData')!);
+    console.log(user);
+
+    return user !== null ? true : false;
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/landing']);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBusNnwU80q6e8HmRmQqOaytYr-4YtKvgo',
+    authDomain: 'kenyantesting.firebaseapp.com',
+    projectId: 'kenyantesting',
+    storageBucket: 'kenyantesting.appspot.com',
+    messagingSenderId: '598823298747',
+    appId: '1:598823298747:web:ce5591d14cd30f5ae82b9e',
+    measurementId: 'G-NDL4N7G3J4',
+  },
 };
 
 /*
