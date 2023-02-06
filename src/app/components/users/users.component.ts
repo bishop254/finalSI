9c01b91ac7b206e3e912fe1aa2a8712a74d9f564
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-02-07 00:38:31 +0300

    Modified our users page

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,33 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { UserAlbumsService } from 'src/app/services/user-albums.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+  users!: Observable<any>;
+  usersAlbCount!: Observable<any>;
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private userServ: UserAlbumsService,
+    private ref: ChangeDetectorRef,
+    private router: Router
+  ) {}
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit() {
+    this.loadUserInfo();
+  }
+
+  loadUserInfo() {
+    this.users = this.userServ.getUsers();
+    // console.log(this.users);
+  }
+
+  userDetails(id: string) {
+    this.router.navigate([`/users/${id}`]);
+  }
 }
