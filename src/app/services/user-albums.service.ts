d05939c14703bcf60b46d43564966a52e1d195fe
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-02-07 02:33:49 +0300

    Added a method that fethes album information

--- src/app/services/user-albums.service.ts
+++ src/app/services/user-albums.service.ts
@@ -27,8 +27,24 @@ export class UserAlbumsService {
       .pipe(
         map((resp) => {
           if (resp) {
-            // console.log(resp, 'albums');
+            console.log(resp, 'albums');
             return { albums: resp, count: (resp as unknown as []).length };
+          }
+          throw new Error('Could not load data');
+        })
+      );
+  }
+
+  getAlbumPhotos(id: string | number): Observable<any> {
+    return this.http
+      .get(
+        `https://jsonplaceholder.typicode.com/albums/${id.toString()}/photos`
+      )
+      .pipe(
+        map((resp) => {
+          if (resp) {
+            console.log(resp, 'albumPhotos');
+            return resp;
           }
           throw new Error('Could not load data');
         })
